1abbba28941b7a8339cd0e14be43f4ba93f0e3eb
Jeremiah Kamama <jeremiahkamama@gmail.com>
2026-04-13 09:39:16 +0300

    updated backend url

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,7 @@ import { OptionsModule } from "./components/OptionsModule";
 import { JournalModule } from "./components/JournalModule";
 import { HomeModule } from "./components/HomeModule";
 
-const BACKEND_URL = import.meta.env.VITE_API_URL || "https://portv1-oijx.onrender.com
+const BACKEND_URL = import.meta.env.VITE_API_URL || "https://zenin-mx6w.onrender.com
   /api";
 
 function App() {
